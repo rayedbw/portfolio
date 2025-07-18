ff20c16beb66d57accd0fddbf9cb4bca6a920e3d
Md Rayed Bin Wahed <169150946+rayedbw@users.noreply.github.com>
2025-07-18 16:32:01 +0000

    Move name and title to the right and decrease h1 font size

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,7 @@ import PropTypes from "prop-types";
  * Home background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  *
@@ -22,18 +22,28 @@ import PropTypes from "prop-types";
  */
 import image from "../images/rayed.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
-
 const Home = ({ name, title }) => {
   return (
     <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+      <img className="background" src={image} alt="Md Rayed Bin Wahed" />
+      <div
+        style={{
+          position: "absolute",
+          top: "5rem",
+          right: "2rem",
+          width: "40rem",
+          textAlign: "right",
+        }}
+      >
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
       <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+        <img
+          src={arrowSvg}
+          style={{ height: "3rem", width: "3rem" }}
+          alt="Arrow pointing downwards, indicating to scroll down for more content"
+        />
       </div>
     </section>
   );
